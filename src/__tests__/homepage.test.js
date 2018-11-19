174f8da1e000680387d4b5aa3eb41ba9df72e86b
Adrian Thompson <adrian.thompson@sky.uk>
2018-11-19 11:16:10 +0000

    whole app is working and first unit tests done

--- src/__tests__/homepage.test.js
+++ src/__tests__/homepage.test.js
@@ -2,12 +2,27 @@ import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import { shallow } from 'enzyme';
 
-import { HomePage } from '../containers';
+import { HomePage, PhotoGallery } from '../containers';
 
 describe('Homepage', () => {
-  it('should render without exploding', () => {
+  it('should render the HomePage container without exploding...', () => {
       const wrapper = shallow(<HomePage />);
-      expect(true).toEqual(true);
+      expect(wrapper).toBeTruthy();
+      expect(wrapper.find('div').length).toBe(1);
+      expect(wrapper.find('h1').length).toBe(1);
+  });
+  it('should render the expected text', () => {
+      const wrapper = shallow(<HomePage />);
+      const expectedText = "Photo Selector";
+      expect(wrapper.find('h1').text()).toEqual(expectedText);
+  });
+  it('should render the expected children', () => {
+    const wrapper = shallow((
+      <HomePage>
+        <PhotoGallery />
+      </HomePage>
+    ))
+      expect(wrapper.contains(<PhotoGallery />)).toBe(true);
   });
 });
 
